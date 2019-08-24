a5230105b0e77dad7c1ead185c504d8d49212bbe
Evan Roberto <3_van@live.com>
2019-08-25 01:33:31 +0800

    UPDATE: kanban

--- src/components/kanban/Kanban.js
+++ src/components/kanban/Kanban.js
@@ -1,23 +1,51 @@
 import React from "react";
 import './Kanban.css';
 
+const categories = ['todo', 'doing', 'completed', 'qa', 'closed'];
+const categoryLabels = {
+  todo: 'To-do',
+  doing: 'Doing',
+  completed: 'Completed',
+  qa: 'QA',
+  closed: 'Closed'
+};
+
+const tasks = [
+  { id: 1, content: 'A feature requested from user to improve overall efficiency.', status: 'backlog' },
+  { id: 2, content: 'Enhancement requested from product owner to improve overall design.', status: 'backlog' }
+];
+
 class Kanban extends React.Component {
   render() {
     return (
       <div className="kanban">
         <div className="row">
-          <div className="col"><div className="board"><div className="board-header">To-do</div></div></div>
-          <div className="col"><div className="board"><div className="board-header">Doing</div></div></div>
-          <div className="col"><div className="board"><div className="board-header">Completed</div></div></div>
-          <div className="col"><div className="board"><div className="board-header">QA</div></div></div>
-          <div className="col"><div className="board"><div className="board-header">Closed</div></div></div>
+          {
+            categories.map(category => (
+              <div className="col">
+                <div className="board">
+                  <div className="board-header">
+                    {categoryLabels[category]}
+                  </div>
+                </div>
+              </div>
+            ))
+          }
         </div>
+
         <div className="row">
           <div className="col">
             <div className="issue-board">
               <div className="board-header">Backlog</div>
-              <div className="issue" draggable="true">A feature requested from user to improve overall efficiency.</div>
-              <div className="issue" draggable="true">Enhancement requested from product owner to improve overall design.</div>
+              {
+                tasks
+                  .filter(task => task.status === 'backlog')
+                  .map(task => (
+                  <div className="issue" draggable="true">
+                    {task.content}
+                  </div>
+                ))
+              }
             </div>
           </div>
         </div>
